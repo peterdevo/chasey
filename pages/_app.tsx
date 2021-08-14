494bb83a4616e600a166f0e84b5191fdb930072e
joke <joke@gmail.com>
2021-08-15 00:21:02 +0200

    Payment
    implement stripe payment for frontend and backend

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
 import "../styles/globals.css";
 import { Provider } from "next-auth/client";
 import { ShoppingCartWrapper } from "../context/ShoppingCartContext";
-
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_SHAREKEY,{ locale: 'en'});
 function MyApp({ Component, pageProps }) {
   return (
+    
     <Provider session={pageProps.session}>
+      <Elements stripe={stripePromise}>
       <ShoppingCartWrapper>
         <Component {...pageProps} />
       </ShoppingCartWrapper>
+      </Elements>
     </Provider>
   );
 }
--- pages/checkout-page/[id].tsx
+++ pages/checkout-page/[id].tsx
@@ -1,94 +1,12 @@
 import Checkout from "../../components/pagescontainers/Checkout";
-import { useShoppingCartContext } from "../../context/ShoppingCartContext";
 import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from "next";
-import { useState } from "react";
-import { useRouter } from "next/router";
 import User from "../../models/UserDetail";
 import dbConnect from "../../utils/config";
-import { checkoutOrderSchema } from "../../validationSchemas/checkoutOrder";
-import SuccessMessage from "../../components/reused/SuccessMessage";
-import { RiArrowGoBackLine } from "react-icons/ri";
 
 const CheckOut = ({ userData }) => {
-  const ShoppingCartContext = useShoppingCartContext();
-  const products = ShoppingCartContext.shoppingCart;
-  const [errorMessage, setErrorMessage] = useState("");
-  const [isSucess, setIsSuccess] = useState(false);
-  const route = useRouter();
-  const onCheckout = async (personalInfo) => {
-    try {
-      const postData = async () => {
-        const order = {
-          personalInfo: {
-            name: personalInfo.name,
-            email: personalInfo.email,
-            phone: personalInfo.phone,
-            address: {
-              city: personalInfo.city,
-              street: personalInfo.street,
-              zipCode: personalInfo.zipCode,
-            },
-            userId: personalInfo.userId,
-          },
-          products,
-        };
-
-        await checkoutOrderSchema.validate(order);
-
-        const response = await fetch("/api/add-order", {
-          method: "POST",
-          body: JSON.stringify(order),
-          headers: {
-            "Content-Type": "Application/json",
-          },
-        });
-
-        return response.json();
-      };
-
-      const data = await postData();
-
-      if (data.errors) {
-        setErrorMessage(data.errors[0]);
-      } else {
-        setIsSuccess(true);
-        ShoppingCartContext.setShoppingCart([]);
-        ShoppingCartContext.setTotalPriceAndAmount({
-          totalprice: 0,
-          totaAmount: 0,
-        });
-      }
-    } catch (error) {
-      setErrorMessage(error.errors[0]);
-    }
-  };
   return (
     <div>
-      {isSucess && (
-        <SuccessMessage>
-          <div>
-            <div>Thank you for your order!</div>
-            <div
-              style={{
-                marginTop: "10px",
-                color: "black",
-                cursor: "pointer",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-              onClick={() => route.push("/")}
-            >
-              <RiArrowGoBackLine size={20} style={{marginRight:"5px"}} /> Back Home
-            </div>
-          </div>
-        </SuccessMessage>
-      )}
-      <Checkout
-        errorMessage={errorMessage}
-        userData={userData}
-        onCheckout={(personalInfo) => onCheckout(personalInfo)}
-      />
+      <Checkout userData={userData} />
     </div>
   );
 };
@@ -133,7 +51,7 @@ export const getStaticProps: GetStaticProps = async (
         zipCode: response.address.zipCode,
       },
     },
-    revalidate: 2,
+    revalidate: 1,
   };
 };
 
