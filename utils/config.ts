9de80c6d33767a4a2b0310b88831955a49cbac78
joke <joke@gmail.com>
2021-08-20 21:59:26 +0200

    fix bug

--- utils/config.ts
+++ utils/config.ts
@@ -1,24 +1,14 @@
 import mongoose from 'mongoose'
 const globalAny:any = global;
-/** 
-Source : 
-https://github.com/vercel/next.js/blob/canary/examples/with-mongodb-mongoose/utils/dbConnect.js 
-**/
 
+// const MONGODB_URI = process.env.DB_HOST
 
-const MONGODB_URI = process.env.DB_HOST
+// if (!MONGODB_URI) {
+//   throw new Error(
+//     'Please define the MONGODB_URI environment variable inside .env.local'
+//   )
+// }
 
-if (!MONGODB_URI) {
-  throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env.local'
-  )
-}
-
-/**
- * Global is used here to maintain a cached connection across hot reloads
- * in development. This prevents connections growing exponentially
- * during API Route usage.
- */
 let cached = globalAny.mongoose
 
 if (!cached) {
@@ -40,7 +30,7 @@ async function dbConnect () {
       useCreateIndex: true
     }
 
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then(mongoose => {
+    cached.promise = mongoose.connect(process.env.DB_HOST, opts).then(mongoose => {
       return mongoose
     })
   }
