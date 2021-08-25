8e30b33cfcf18053522d9c6337e0efe4826bacb2
joke <joke@gmail.com>
2021-08-25 11:35:54 +0200

    debug send forget password link

--- pages/api/sendResetPasswordLink.ts
+++ pages/api/sendResetPasswordLink.ts
@@ -10,33 +10,33 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if ((req.method = "POST")) {
     try {
       const isUser = await User.findOne({ email });
-      
-      if (isUser) {
-        const token = JWT.sign(
-          { id: isUser._id, email: isUser.email },
-          process.env.JWT_SECRET,
-          { expiresIn: "15m" }
-        );
-        let transporter = nodemailer.createTransport({
-          host: "smtp.gmail.com",
-          port: 465,
-          secure: true,
-          auth: {
-            user: process.env.EMAIL_SECRET,
-            pass: process.env.PASS_SECRET,
-          },
-        });
+      res.status(500).json({ message: "Successfully",err:true });
+      // if (isUser) {
+      //   const token = JWT.sign(
+      //     { id: isUser._id, email: isUser.email },
+      //     process.env.JWT_SECRET,
+      //     { expiresIn: "15m" }
+      //   );
+      //   let transporter = nodemailer.createTransport({
+      //     host: "smtp.gmail.com",
+      //     port: 465,
+      //     secure: true,
+      //     auth: {
+      //       user: process.env.EMAIL_SECRET,
+      //       pass: process.env.PASS_SECRET,
+      //     },
+      //   });
 
-        let mailOptions = {
-          from: "Chasey",
-          to: email,
-          subject: "Reset password",
-          html: `<html><body> <a href=${req.headers.origin}/reset-password/${isUser.id}/${token}>Reset your password here.</a> </body>`,
-        };
-        await transporter.sendMail(mailOptions);
+      //   let mailOptions = {
+      //     from: "Chasey",
+      //     to: email,
+      //     subject: "Reset password",
+      //     html: `<html><body> <a href=${req.headers.origin}/reset-password/${isUser.id}/${token}>Reset your password here.</a> </body>`,
+      //   };
+      //   await transporter.sendMail(mailOptions);
 
-        res.status(200).json({ message: "The link has been sent to you email." });
-      } else res.status(404).json({ message: "This email does not exist.",err:true });
+      //   res.status(200).json({ message: "The link has been sent to you email." });
+      // } else res.status(404).json({ message: "This email does not exist.",err:true });
     } catch (error) {
       res.status(500).json({ message: error,err:true });
     }
